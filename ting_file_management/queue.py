8e36e2a726651b1e258c9a53f929d0c175083223
hildelio <hildelio@gmail.com>
2023-09-04 20:55:53 -0300

    Projeto Trybe is not Google (TING) 🚫 🔍 - 🔋, 🚶 🚶 🚶 - 🐍 Req 01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = []
+        self.__length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.__length
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
+        self.__length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.__length > 0:
+            self.__length -= 1
+            return self.data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < self.__length:
+            return self.data[index]
+        else:
+            raise IndexError("Índice Inválido ou Inexistente")
