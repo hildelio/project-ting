3846cb99bdb317758452430764da5313f20fd2bb
hildelio <hildelio@gmail.com>
2023-09-05 09:25:21 -0300

    Projeto Trybe is not Google (TING) 🚫 🔍 - 🔋, 🚶 🚶 🚶 - 🐍 Req 03, 04 e 05

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,31 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+    if len(instance.data) > 0:
+        if (items["nome_do_arquivo"] == path_file for items in instance.data):
+            return None
+    instance.enqueue(data)
+    sys.stdout.write(f"{data}\n")
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance.data) == 0:
+        return sys.stdout.write("Não há elementos\n")
+    sys.stdout.write(
+        f"Arquivo {instance.dequeue()['nome_do_arquivo']} "
+        "removido com sucesso\n"
+    )
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position not in range(len(instance.data)):
+        return sys.stderr.write("Posição inválida\n")
+    sys.stdout.write(f"{instance.search(position)}\n")
