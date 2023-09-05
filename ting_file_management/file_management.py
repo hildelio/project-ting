dbc8a2f96c231a17366b4d45f98066525404e084
hildelio <hildelio@gmail.com>
2023-09-04 21:10:50 -0300

    Projeto Trybe is not Google (TING) 🚫 🔍 - 🔋, 🚶 🚶 🚶 - 🐍 Req 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            return sys.stderr.write("Formato inválido")
+        with open(path_file, "r") as file:
+            return [line.strip() for line in file.readlines()]
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
