15a2ca36bd212440b6a7beefc45569a2dae9e0cb
hildelio <hildelio@gmail.com>
2023-09-05 10:07:09 -0300

    Projeto Trybe is not Google (TING) 🚫 🔍 - 🔋, 🚶 🚶 🚶 - 🐍 Req 07 parcial

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for i in range(len(instance)):
+        file_info = instance.search(i)
+        file_name = file_info["nome_do_arquivo"]
+        lines = file_info["linhas_do_arquivo"]
+
+        occurrences = [
+            {"linha": j + 1}
+            for j, line in enumerate(lines)
+            if word.lower() in line.lower()
+        ]
+
+        if occurrences:
+            results.append({
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": occurrences
+            })
+
+    return results
 
 
 def search_by_word(word, instance):
